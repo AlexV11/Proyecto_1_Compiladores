96da38206a8f8f1aba3877868c3569fc5e79c94c
Alex Villalba <aalexvillalba11@gmail.com>
2023-03-07 22:31:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ tokens = ['LENG', 'OTRO']
 t_LENG = r'a(b|c)*dcc(b|d)+aa'
 t_OTRO = r'.'
 
-st.write(
-    '''
-    338803 - Saul Fernando Rodríguez Gutiérrez
-    338817 - Eric Alejandro Aguilar Marcial
-    338931 - Andrés Alexis Villalba García
-    '''
-)
-
 def revisar(cadena):
     aceptador = lex.lex()
     aceptador.input(cadena)
@@ -26,6 +18,9 @@ def revisar(cadena):
 
 st.title("Proyecto")
 st.write("Sea un lenguaje como en L1 = a(b|c)∗d y sea un segundo lenguaje como en L2 = cc(b|d)+aa. Hagamos un aceptador de cadenas para el lenguaje L3 = L1L2. El aceptador de cadenas debe pedir la cadena al usuario y responder si le acepta o no lo hace.")
+st.write('338803 - Saul Fernando Rodríguez Gutiérrez')
+st.write('338817 - Eric Alejandro Aguilar Marcial')
+st.write('338931 - Andrés Alexis Villalba García')
 
 st.text_input("Ingrese una cadena:", key="cadena")
 if st.button("Revisar"):
